bde4e675dab280e1cb00e678d396880a1bd51180
William Alfonso Fuentes <fuentesw3008@gmail.com>
2024-08-09 17:43:17 -0500

    nodemon fix

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ import express from 'express';
 
 const app = express();
 
-app.get('/recovery', (req, res) => {
-    try {
-        const data = axios.get('https://ajoaquinlizarraga.github.io/apipublic/mydata/people/people.json');
-    } catch (error) {
+// app.get('/recovery', (req, res) => {
+//     try {
+//         const data = axios.get('https://ajoaquinlizarraga.github.io/apipublic/mydata/people/people.json');
+//     } catch (error) {
 
-    }
-})
+//     }
+// })
 
 export default app;
